6c705bcfd7f3498df37ee53b004e62351f573d34
Alejandro Avagnina <avatxus@gmail.com>
2023-12-12 16:24:56 -0300

    chore: kupo podmonitor

--- bootstrap/instance/monitor.tf
+++ bootstrap/instance/monitor.tf
@@ -1,29 +1,29 @@
-# resource "kubernetes_manifest" "instance_monitor" {
-#   manifest = {
-#     apiVersion = "monitoring.coreos.com/v1"
-#     kind       = "PodMonitor"
-#     metadata = {
-#       labels = {
-#         "app.kubernetes.io/component" = "o11y"
-#         "app.kubernetes.io/part-of"   = "demeter"
-#       }
-#       name      = local.instance
-#       namespace = var.namespace
-#     }
-#     spec = {
-#       selector = {
-#         matchLabels = {
-#           "demeter.run/instance"        = var.instance_name
-#           "demeter.run/salt"            = var.salt
-#           "cardano.demeter.run/network" = var.network
-#         }
-#       }
-#       podMetricsEndpoints = [
-#         {
-#           port = "server",
-#           path = "/metrics"
-#         }
-#       ]
-#     }
-#   }
-# }
+resource "kubernetes_manifest" "instance_monitor" {
+  manifest = {
+    apiVersion = "monitoring.coreos.com/v1"
+    kind       = "PodMonitor"
+    metadata = {
+      labels = {
+        "app.kubernetes.io/component" = "o11y"
+        "app.kubernetes.io/part-of"   = "demeter"
+      }
+      name      = local.instance_name
+      namespace = var.namespace
+    }
+    spec = {
+      selector = {
+        matchLabels = {
+          "demeter.run/instance"            = local.instance_name
+          "cardano.demeter.run/network"     = var.network
+          "cardano.demeter.run/kupo-pruned" = var.pruned ? "true" : "false"
+        }
+      }
+      podMetricsEndpoints = [
+        {
+          port = "http",
+          path = "/health"
+        }
+      ]
+    }
+  }
+}
