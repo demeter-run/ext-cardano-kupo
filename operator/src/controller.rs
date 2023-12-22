eb0f7993b3fd1bbe8842d29b919c808741653887
paulobressan <paulo.bressan@outlook.com>
2023-12-22 17:50:04 -0300

    chore: removed kube finalizer

--- operator/src/controller.rs
+++ operator/src/controller.rs
@@ -5,12 +5,7 @@ use crate::{
 };
 use futures::StreamExt;
 use kube::{
-    runtime::{
-        controller::Action,
-        finalizer::{finalizer, Event},
-        watcher::Config as WatcherConfig,
-        Controller,
-    },
+    runtime::{controller::Action, watcher::Config as WatcherConfig, Controller},
     Api, Client, CustomResource, ResourceExt,
 };
 use schemars::JsonSchema;
@@ -63,24 +58,6 @@ pub struct KupoPortStatus {
     pub auth_token: Option<String>,
 }
 
-impl KupoPort {
-    async fn reconcile(&self, ctx: Arc<Context>) -> Result<Action> {
-        let client = ctx.client.clone();
-        let namespace = self.namespace().unwrap();
-
-        let private_dns_service_name =
-            build_private_dns_service_name(&self.spec.network, self.spec.prune_utxo);
-        handle_reference_grant(client.clone(), &namespace, self, &private_dns_service_name).await?;
-        handle_http_route(client.clone(), &namespace, self, &private_dns_service_name).await?;
-        handle_auth(client.clone(), &namespace, self).await?;
-        Ok(Action::requeue(Duration::from_secs(5 * 60)))
-    }
-
-    async fn cleanup(&self, _: Arc<Context>) -> Result<Action> {
-        Ok(Action::await_change())
-    }
-}
-
 fn build_private_dns_service_name(network: &Network, prune_utxo: bool) -> String {
     if prune_utxo {
         return format!("kupo-{}-pruned", network);
@@ -89,17 +66,16 @@ fn build_private_dns_service_name(network: &Network, prune_utxo: bool) -> String
 }
 
 async fn reconcile(crd: Arc<KupoPort>, ctx: Arc<Context>) -> Result<Action> {
-    let ns = crd.namespace().unwrap();
-    let crds: Api<KupoPort> = Api::namespaced(ctx.client.clone(), &ns);
+    let client = ctx.client.clone();
+    let namespace = crd.namespace().unwrap();
 
-    finalizer(&crds, KUPO_PORT_FINALIZER, crd, |event| async {
-        match event {
-            Event::Apply(crd) => crd.reconcile(ctx.clone()).await,
-            Event::Cleanup(crd) => crd.cleanup(ctx.clone()).await,
-        }
-    })
-    .await
-    .map_err(|e| Error::FinalizerError(Box::new(e)))
+    let private_dns_service_name =
+        build_private_dns_service_name(&crd.spec.network, crd.spec.prune_utxo);
+    handle_reference_grant(client.clone(), &namespace, &crd, &private_dns_service_name).await?;
+    handle_http_route(client.clone(), &namespace, &crd, &private_dns_service_name).await?;
+    handle_auth(client.clone(), &namespace, &crd).await?;
+
+    Ok(Action::await_change())
 }
 
 fn error_policy(crd: Arc<KupoPort>, err: &Error, ctx: Arc<Context>) -> Action {
