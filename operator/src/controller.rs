33735dc008268b086d5fde04c23f31a1757fe3fb
Felipe Gonzalez <gonzalezz_felipe@hotmail.com>
2024-03-04 14:23:08 -0300

    chore: Change endpoint to include major version (#30)
    * chore: Change endpoint to include major version
    
    * Remove unused imports

--- operator/src/controller.rs
+++ operator/src/controller.rs
@@ -49,6 +49,7 @@ pub struct KupoPortSpec {
     pub prune_utxo: bool,
     // throughput should be 0, 1, 2
     pub throughput_tier: String,
+    pub kupo_version: Option<String>,
 }
 
 #[derive(Deserialize, Serialize, Clone, Default, Debug, JsonSchema)]
@@ -62,7 +63,7 @@ pub struct KupoPortStatus {
 async fn reconcile(crd: Arc<KupoPort>, ctx: Arc<Context>) -> Result<Action> {
     let key = handle_auth(&ctx.client, &crd).await?;
 
-    let (hostname, hostname_key) = build_hostname(&crd.spec.network, &key);
+    let (hostname, hostname_key) = build_hostname(&crd.spec.network, &key, &crd.spec.kupo_version);
 
     let status = KupoPortStatus {
         endpoint_url: format!("https://{hostname}",),
