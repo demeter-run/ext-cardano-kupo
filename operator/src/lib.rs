f212a9bdd3cc1ef44c9da0bdca457d855c506cf6
paulobressan <paulo.bressan@outlook.com>
2024-03-21 15:38:05 -0300

    chore(operator): added old kong metrics collector

--- operator/src/lib.rs
+++ operator/src/lib.rs
@@ -41,6 +41,12 @@ impl From<kube::Error> for Error {
     }
 }
 
+impl From<reqwest::Error> for Error {
+    fn from(value: reqwest::Error) -> Self {
+        Error::HttpError(value.to_string())
+    }
+}
+
 #[derive(Clone)]
 pub struct State {
     registry: Registry,
