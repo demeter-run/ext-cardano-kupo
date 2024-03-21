8b90e98e9f7db747113ba82d19f3c54b08ee5083
Paulo Bressan <paulo.bressan@outlook.com>
2024-03-21 15:42:16 -0300

    Support old kong metrics collector (#41)

--- operator/src/metrics.rs
+++ operator/src/metrics.rs
@@ -8,9 +8,9 @@ use regex::Regex;
 use serde::{Deserialize, Deserializer};
 use std::{net::SocketAddr, str::FromStr, sync::Arc};
 use tokio::net::TcpListener;
-use tracing::{error, info, instrument};
+use tracing::{error, info, instrument, warn};
 
-use crate::{get_config, Error, KupoPort, State};
+use crate::{get_config, Config, Error, KupoPort, State};
 
 #[derive(Clone)]
 pub struct Metrics {
@@ -87,96 +87,23 @@ impl Metrics {
     }
 }
 
-#[instrument("metrics collector run", skip_all)]
-pub fn run_metrics_collector(state: Arc<State>) {
-    tokio::spawn(async move {
-        info!("collecting metrics running");
+async fn api_get_metrics(
+    state: Arc<State>,
+) -> Result<Response<BoxBody<Bytes, hyper::Error>>, hyper::Error> {
+    let metrics = state.metrics_collected();
 
-        let config = get_config();
-        let client = reqwest::Client::builder().build().unwrap();
-        let project_regex = Regex::new(r"prj-(.+)\..+").unwrap();
-        let network_regex = Regex::new(r"kupo-([\w]+)-.+").unwrap();
-        let mut last_execution = Utc::now();
+    let encoder = TextEncoder::new();
+    let mut buffer = vec![];
+    encoder.encode(&metrics, &mut buffer).unwrap();
 
-        loop {
-            tokio::time::sleep(config.metrics_delay).await;
-
-            let end = Utc::now();
-            let start = (end - last_execution).num_seconds();
-
-            last_execution = end;
-
-            let query = format!(
-                "sum by (consumer, exported_instance) (increase(kupo_proxy_http_total_request{{status_code!~\"401|429|503\"}}[{start}s] @ {}))",
-                end.timestamp_millis() / 1000
-            );
-
-            let result = client
-                .get(format!("{}/query?query={query}", config.prometheus_url))
-                .send()
-                .await;
-
-            if let Err(err) = result {
-                error!(error = err.to_string(), "error to make prometheus request");
-                state
-                    .metrics
-                    .metrics_failure(&Error::HttpError(err.to_string()));
-                continue;
-            }
-
-            let response = result.unwrap();
-            let status = response.status();
-            if status.is_client_error() || status.is_server_error() {
-                error!(status = status.to_string(), "request status code fail");
-                state.metrics.metrics_failure(&Error::HttpError(format!(
-                    "Prometheus request error. Status: {} Query: {}",
-                    status, query
-                )));
-                continue;
-            }
-
-            let response = response.json::<PrometheusResponse>().await.unwrap();
-            for result in response.data.result {
-                if result.value == 0.0
-                    || result.metric.consumer.is_none()
-                    || result.metric.exported_instance.is_none()
-                {
-                    continue;
-                }
-
-                let consumer = result.metric.consumer.unwrap();
-                let project_captures = project_regex.captures(&consumer);
-                if project_captures.is_none() {
-                    continue;
-                }
-                let project_captures = project_captures.unwrap();
-                let project = project_captures.get(1).unwrap().as_str();
-
-                let instance = result.metric.exported_instance.unwrap();
-                let network_captures = network_regex.captures(&instance);
-                if network_captures.is_none() {
-                    continue;
-                }
-                let network_captures = network_captures.unwrap();
-                let network = network_captures.get(1).unwrap().as_str();
-
-                let dcu_per_request = config.dcu_per_request.get(network);
-                if dcu_per_request.is_none() {
-                    let error = Error::ConfigError(format!(
-                        "dcu_per_request not configured to {} network",
-                        network
-                    ));
-                    error!(error = error.to_string());
-                    state.metrics.metrics_failure(&error);
-                    continue;
-                }
-                let dcu_per_request = dcu_per_request.unwrap();
-
-                let dcu = result.value * dcu_per_request;
-                state.metrics.count_dcu_consumed(project, network, dcu);
-            }
-        }
-    });
+    let res = Response::builder()
+        .body(
+            Full::new(buffer.into())
+                .map_err(|never| match never {})
+                .boxed(),
+        )
+        .unwrap();
+    Ok(res)
 }
 
 pub fn run_metrics_server(state: Arc<State>) {
@@ -224,23 +151,171 @@ pub fn run_metrics_server(state: Arc<State>) {
     });
 }
 
-async fn api_get_metrics(
-    state: Arc<State>,
-) -> Result<Response<BoxBody<Bytes, hyper::Error>>, hyper::Error> {
-    let metrics = state.metrics_collected();
+async fn collect_prometheus_metrics(
+    config: &Config,
+    query: String,
+) -> Result<PrometheusResponse, Error> {
+    let client = reqwest::Client::builder().build().unwrap();
 
-    let encoder = TextEncoder::new();
-    let mut buffer = vec![];
-    encoder.encode(&metrics, &mut buffer).unwrap();
+    let response = client
+        .get(format!("{}/query?query={query}", config.prometheus_url))
+        .send()
+        .await?;
 
-    let res = Response::builder()
-        .body(
-            Full::new(buffer.into())
-                .map_err(|never| match never {})
-                .boxed(),
-        )
-        .unwrap();
-    Ok(res)
+    let status = response.status();
+    if status.is_client_error() || status.is_server_error() {
+        error!(status = status.to_string(), "request status code fail");
+        return Err(Error::HttpError(format!(
+            "Prometheus request error. Status: {} Query: {}",
+            status, query
+        )));
+    }
+
+    Ok(response.json().await.unwrap())
+}
+
+#[instrument("metrics collector run", skip_all)]
+pub fn run_metrics_collector(state: Arc<State>) {
+    tokio::spawn(async move {
+        info!("collecting metrics running");
+
+        let config = get_config();
+        let project_regex = Regex::new(r"prj-(.+)\..+").unwrap();
+        let network_regex = Regex::new(r"kupo-([\w]+)-.+").unwrap();
+        let mut last_execution = Utc::now();
+
+        loop {
+            tokio::time::sleep(config.metrics_delay).await;
+
+            let end = Utc::now();
+            let start = (end - last_execution).num_seconds();
+
+            last_execution = end;
+
+            let query = format!(
+                "sum by (consumer, exported_instance) (increase(kupo_proxy_http_total_request{{status_code!~\"401|429|503\"}}[{start}s] @ {}))",
+                end.timestamp_millis() / 1000
+            );
+
+            let response = collect_prometheus_metrics(config, query).await;
+            if let Err(err) = response {
+                error!(error = err.to_string(), "error to make prometheus request");
+                state.metrics.metrics_failure(&err);
+                continue;
+            }
+            let response = response.unwrap();
+
+            for result in response.data.result {
+                if result.value == 0.0
+                    || result.metric.consumer.is_none()
+                    || result.metric.exported_instance.is_none()
+                {
+                    continue;
+                }
+
+                let consumer = result.metric.consumer.unwrap();
+                let project_captures = project_regex.captures(&consumer);
+                if project_captures.is_none() {
+                    warn!(consumer, "invalid project to the regex");
+                    continue;
+                }
+                let project_captures = project_captures.unwrap();
+                let project = project_captures.get(1).unwrap().as_str();
+
+                let instance = result.metric.exported_instance.unwrap();
+                let network_captures = network_regex.captures(&instance);
+                if network_captures.is_none() {
+                    warn!(instance, "invalid network to the regex");
+                    continue;
+                }
+                let network_captures = network_captures.unwrap();
+                let network = network_captures.get(1).unwrap().as_str();
+
+                let dcu_per_request = config.dcu_per_request.get(network);
+                if dcu_per_request.is_none() {
+                    let error = Error::ConfigError(format!(
+                        "dcu_per_request not configured to {} network",
+                        network
+                    ));
+                    error!(error = error.to_string());
+                    state.metrics.metrics_failure(&error);
+                    continue;
+                }
+                let dcu_per_request = dcu_per_request.unwrap();
+
+                let dcu = result.value * dcu_per_request;
+                state.metrics.count_dcu_consumed(project, network, dcu);
+            }
+        }
+    });
+}
+
+#[instrument("kong metrics collector run", skip_all)]
+pub fn run_kong_metrics_collector(state: Arc<State>) {
+    tokio::spawn(async move {
+        info!("collecting kong metrics running");
+
+        let config = get_config();
+        let regex = Regex::new(r"(.+)\.(.+)-.+").unwrap();
+        let mut last_execution = Utc::now();
+
+        loop {
+            tokio::time::sleep(config.metrics_delay).await;
+
+            let end = Utc::now();
+            let start = (end - last_execution).num_seconds();
+
+            last_execution = end;
+
+            let query = format!(
+                    "sum by (consumer) (increase(kong_http_requests_total{{service='kupo-v1-ingress-kong-proxy', code!~\"429|401|503\"}}[{start}s] @ {}))",
+                    end.timestamp_millis() / 1000
+                );
+
+            let response = collect_prometheus_metrics(config, query).await;
+            if let Err(err) = response {
+                error!(error = err.to_string(), "error to make prometheus request");
+                state.metrics.metrics_failure(&err);
+                continue;
+            }
+            let response = response.unwrap();
+
+            for result in response.data.result {
+                if result.value == 0.0 || result.metric.consumer.is_none() {
+                    continue;
+                }
+
+                let consumer = result.metric.consumer.unwrap();
+
+                let captures = regex.captures(&consumer);
+                if captures.is_none() {
+                    warn!(consumer, "invalid consumer to the regex");
+                    continue;
+                }
+
+                let captures = captures.unwrap();
+                let namespace = captures.get(1).unwrap().as_str();
+                let network = captures.get(2).unwrap().as_str();
+
+                let dcu_per_request = config.dcu_per_request.get(network);
+                if dcu_per_request.is_none() {
+                    let error = Error::ConfigError(format!(
+                        "dcu_per_request not configured to {} network",
+                        network
+                    ));
+                    error!(error = error.to_string());
+                    state.metrics.metrics_failure(&error);
+                    continue;
+                }
+                let dcu_per_request = dcu_per_request.unwrap();
+
+                let dcu = result.value * dcu_per_request;
+
+                let project = namespace.split_once("prj-").unwrap().1;
+                state.metrics.count_dcu_consumed(project, network, dcu);
+            }
+        }
+    });
 }
 
 #[derive(Debug, Deserialize)]
